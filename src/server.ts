1288a239c6cd9b934ef68037b9982b340bc24bb0
Reza <forhad.bimt@gmail.com>
2026-05-18 16:09:46 +0600

    added vercel configuration

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,17 @@
 import { app } from './app.js';
 import { env } from './config/env.js';
 
-const server = app.listen(env.PORT, () => {
-   console.log(
-      `Quran API listening on port ${env.PORT} in ${env.NODE_ENV} mode`
-   );
-});
-
-const shutdown = (signal: NodeJS.Signals): void => {
-   console.log(`${signal} received. Closing HTTP server...`);
-
-   server.close((error) => {
-      if (error) {
-         console.error('HTTP server shutdown failed', error);
-         process.exit(1);
-      }
-
-      console.log('HTTP server closed');
-      process.exit(0);
+if (process.env.VERCEL !== '1') {
+   app.listen(env.PORT, () => {
+      console.log(`Quran API running`);
    });
-};
+}
 
-process.on('SIGTERM', shutdown);
-process.on('SIGINT', shutdown);
+// const server = app.listen(env.PORT, () => {
+//    console.log(
+//       `Quran API listening on port ${env.PORT} in ${env.NODE_ENV} mode`
+//    );
+// });
+
+
+
